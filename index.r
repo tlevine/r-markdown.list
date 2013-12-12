eaf0d22602643909e458f037937e4d3ba9ce2d55
Thomas Levine <occurrence@thomaslevine.com>
2013-12-12 20:41:49 +0000

    test interactively

--- index.r
+++ index.r
@@ -1,7 +1,7 @@
 exports <- list()
 exports$ol <- function(list.items) {
-  paste0('\n', paste0(seq(1,length(list.items)), '. ', list.items, '\n'), '\n')
+  paste0('\n', paste(paste0(seq(1,length(list.items)), '. ', list.items), collapse = '\n'), '\n\n')
 }
 exports$ul <- function(list.items) {
-  paste0('\n', paste0('* ', list.items, '\n'), '\n')
+  paste0('\n', paste(paste0('* ', list.items), collapse = '\n'), '\n\n')
 }
